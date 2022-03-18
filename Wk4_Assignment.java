0d47798bd8ed8d66ac04f2119058edf0ed9b2423
yen0224 <yen0224@pm.me>
2022-03-18 17:56:47 +0800

    wk4 update

--- Wk4_Assignment.java
+++ Wk4_Assignment.java
@@ -6,33 +6,35 @@ public class Wk4_Assignment {
         String endtext = "\n要繼續嗎";
         String text1, text2 = "選取一個選項";
         int a, b, c;
-        JOptionPane.showConfirmDialog(null, "開始完水果拉霸", "選取一個選項", JOptionPane.YES_NO_CANCEL_OPTION);
-        do {
-            text1 = "";
-            a = spin();
-            b = spin();
-            c = spin();
-            if (a == 0) {
-                text1 += "草莓 ";
-            } else {
-                text1 += "香蕉 ";
-            }
-            if (b == 0) {
-                text1 += "草莓 ";
-            } else {
-                text1 += "香蕉 ";
-            }
-            if (c == 0) {
-                text1 += "草莓";
-            } else {
-                text1 += "香蕉";
-            }
-            if (a == b && b == c) {
-                text1 += "\n中了！恭喜,";
-            }
-            text1 += endtext;
-        } while (JOptionPane.showConfirmDialog(null, text1, text2,
-                JOptionPane.YES_NO_CANCEL_OPTION) == JOptionPane.YES_OPTION);
+        if (JOptionPane.showConfirmDialog(null, "開始完水果拉霸", "選取一個選項",
+                JOptionPane.YES_NO_CANCEL_OPTION) == JOptionPane.YES_OPTION) {
+            do {
+                text1 = "";
+                a = spin();
+                b = spin();
+                c = spin();
+                if (a == 0) {
+                    text1 += "草莓 ";
+                } else {
+                    text1 += "香蕉 ";
+                }
+                if (b == 0) {
+                    text1 += "草莓 ";
+                } else {
+                    text1 += "香蕉 ";
+                }
+                if (c == 0) {
+                    text1 += "草莓";
+                } else {
+                    text1 += "香蕉";
+                }
+                if (a == b && b == c) {
+                    text1 += "\n中了！恭喜,";
+                }
+                text1 += endtext;
+            } while (JOptionPane.showConfirmDialog(null, text1, text2,
+                    JOptionPane.YES_NO_CANCEL_OPTION) == JOptionPane.YES_OPTION);
+        }
     }
 
     public static int spin() {
